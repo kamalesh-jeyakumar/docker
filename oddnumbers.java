1289d3d1a96464ff188e42e53153a596461f577a
kamalesh-jeyakumar <kamaleshj1211@gmail.com>
2025-03-05 21:13:42 +0530

    Update oddnumbers.java

--- oddnumbers.java
+++ oddnumbers.java
@@ -1,6 +1,6 @@
 public class oddnumbers{
     public static void main(String[] args) {
-        for(int i =1; i<= 100; i++){
+        for(int i =1; i<= 10; i++){
             if(i%2 !=0){
                 System.out.println(i);
             }
